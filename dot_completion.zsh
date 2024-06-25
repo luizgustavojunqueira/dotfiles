a1fe2a9a92815bdfbf99116a2002e517a3b25676
luizgustavojunqueira <luizgustavossj@gmail.com>
2024-06-25 17:47:54 -0400

    Add .completion.zsh

---
+++ dot_completion.zsh
@@ -0,0 +1,12 @@
+
+zstyle ':completion:*' use-cache on
+zstyle ':completion:*' cache-path "$XDG_CACHE_HOME/zsh/.zcompcache"
+
+zstyle ':completion:*:default' list-colors ${(s.:.)LS_COLORS}
+
+zstyle  ':completion:*' format '%F{blue}-- %d --%f'
+zstyle ':completion:*:*:*:*:descriptions' format '%F{green}-- %d --%f'
+zstyle ':completion:*:*:*:*:corrections' format '%F{yellow}!- %d (errors: %e) -!%f'
+zstyle ':completion:*:messages' format ' %F{purple} -- %d --%f'
+zstyle ':completion:*:warnings' format ' %F{red}-- no matches found --%f'
+zstyle ':completion:*:option' format '%F{cyan} %d %f'
